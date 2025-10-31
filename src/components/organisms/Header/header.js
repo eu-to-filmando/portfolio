3a47a59b8927bd02bd8ea38e63062b42ab061aa4
bernardoenock <bernardoenock@argosmonitoramento.com.br>
2025-10-31 16:52:03 -0300

    feat: Add burguer menu

--- src/components/organisms/Header/header.js
+++ src/components/organisms/Header/header.js
@@ -31,11 +31,25 @@ export class Header extends Component {
             }, [link.text])
         ));
 
+        const burgerButton = new Component('button', {
+            attributes: {
+                class: 'burger-menu',
+                'aria-expanded': 'false',
+                'aria-controls': 'header-nav'
+            }
+        }, [
+            new Component('span', { attributes: { class: 'burger-line' } }),
+            new Component('span', { attributes: { class: 'burger-line' } }),
+            new Component('span', { attributes: { class: 'burger-line' } })
+        ]);
+
+        nav.props.attributes.id = 'header-nav';
+        
         const container = new Component('div', {
             attributes: {
                 class: 'header__container'
             }
-        }, [logo, nav]);
+        }, [logo, nav, burgerButton]);
 
         super('header', {
             attributes: {
@@ -43,5 +57,48 @@ export class Header extends Component {
                 id: 'header'
             }
         }, [container]);
+    }
+
+    toggleMenu(event) {
+        const headerElement = event.currentTarget.closest('.header');
+        const navElement = headerElement.querySelector('.header__nav');
+        const buttonElement = event.currentTarget;
+
+        navElement.classList.toggle('is-open');
+
+        const isExpanded = navElement.classList.contains('is-open');
+        buttonElement.setAttribute('aria-expanded', isExpanded);
+    }
+
+    render() {
+        const element = super.render();
+        
+        const navLinks = element.querySelectorAll('.nav-link');
+        const navElement = element.querySelector('.header__nav');
+        const buttonElement = element.querySelector('.burger-menu');
+
+        if (buttonElement) {
+            buttonElement.addEventListener('click', this.toggleMenu.bind(this));
+        }
+
+        if (navLinks.length > 0 && navElement && buttonElement) {
+            navLinks.forEach(link => {
+                link.addEventListener('click', () => {
+                    navElement.classList.remove('is-open');
+                    buttonElement.setAttribute('aria-expanded', 'false');
+                });
+            });
+        }
+
+        document.addEventListener('click', (event) => {
+            if (navElement.classList.contains('is-open') && 
+                !element.contains(event.target)) {
+                
+                navElement.classList.remove('is-open');
+                buttonElement.setAttribute('aria-expanded', 'false');
+            }
+        });
+        
+        return element;
     }
 }
